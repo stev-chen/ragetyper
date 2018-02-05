0c07d93072a2af32bffd6eeac83c2bbb9c8a2974
stevenchen <stev.chen@mail.utoronto.ca>
2018-02-04 23:20:13 -0500

    wrapping works ok, but unwanted line break between spans now. Extremely slow.

--- script.js
+++ script.js
@@ -5,19 +5,44 @@ let curr = document.getElementById("curr")
 document.body.onkeydown = function(e){
     if(e.keyCode == 32 || e.key === ' '){
         word = document.getElementById("field").value
-        document.getElementById("field").value = ""
+        document.getElementById("field").value = " "
     	$('#full').append(document.createTextNode(word));
+    	wrapUp($('#full'), $('#curr'))
+    	wrapUp($('#full'), $('#curr'))
     }
 }
 
-$(function() {
-    $(".title").each(function(i,title) {
-        var width = 0;
-        var originalHeight = $(title).height();
-        var spacer = $('<span style="float:right;height:1px;"/>').prependTo(title);
-        while (originalHeight == $(title).height()) {
-            spacer.width( ++width );
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+function wrapUp(element, curr){
+
+	console.log('starting')
+
+    string = (element.text() + ' ' + curr.text()).split('');
+    element.text('');
+    reversed = string.reverse();
+    var height = 0;
+    for (var i=0;i<reversed.length;i++){
+        element.text(element.text()+reversed[i]);
+        if (height != element.height()){
+            reversed.splice(i,0,"<br/>");
+            i++;
         }
-        spacer.width( --width );
-    });
-});
+        height = element.height();
+        console.log(element.height());
+    }
+    //console.log(reversed);
+    if (reversed[0] == ' '){
+    	reversed.shift();
+    }
+    reversed.shift();
+    extra = reversed.reverse().join('')
+    regCurrText = curr.text().split('').join('(<br/>)*')
+    proper = extra.replace(new RegExp(escapeRegExp(curr.text() + '$'), '')
+    stripped = proper.replace(/\s+$/, '');
+    //console.log(stripped)
+    element.html(stripped)
+    console.log('finished!')
+}
